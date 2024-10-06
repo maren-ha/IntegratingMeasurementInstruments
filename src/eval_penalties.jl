0f7edfc421ea9e6cdb072af84c34c6265a604e96
Maren Hackenberg <maren@imbi.uni-freiburg.de>
2024-10-06 10:26:25 +0200

    removed unused functions, improved comments

--- src/eval_penalties.jl
+++ src/eval_penalties.jl
@@ -106,7 +106,7 @@ end
 
 Fit baseline model for comparison with the ODE model in the latent space: 
 a linear model, where the intercept is learned from baseline variables, with time and measurement instrument
-    as further covariates
+as further covariates
 
 # Arguments
 - `df_all::DataFrame`: DataFrame containing the ODE solutions, latent values, time points, patient IDs, and baseline variables
@@ -121,7 +121,7 @@ function fit_baseline_model(df_all::DataFrame, n_baseline_vars; verbose::Bool=fa
 
     lm_df = deepcopy(df_all)
     nancols = []
-    # standardize each column 
+    # standardize each column, columns with zero variance are later removed from the model
     for ind in 1:n_baseline_vars
         mycol = lm_df[!,Symbol("x$ind")]
         mycol = (mycol .- mean(mycol)) ./ std(mycol)
@@ -131,7 +131,7 @@ function fit_baseline_model(df_all::DataFrame, n_baseline_vars; verbose::Bool=fa
         lm_df[!,Symbol("x$ind")] = mycol
     end
     verbose && @info nancols
-    #nancols # for zolg: 4, 11, 13, 14, 16, 19
+    #nancols for zolg: 4, 11, 13, 14, 16, 19
 
     # convert all columns except measurement and patient_id to Float64
     for colname in names(lm_df)
@@ -139,7 +139,7 @@ function fit_baseline_model(df_all::DataFrame, n_baseline_vars; verbose::Bool=fa
             lm_df[!,colname] = Float64.(lm_df[!,colname])
         end
     end
-    # risdiplam: no x23, zolgensma: no x21
+    # fit  linear models
     if dataset == "all"
         linear_fm_1 = @formula(dim1 ~ 1 + t + measurement + 
             x1 + x2 + x3 + x4 + x5 + x6 + x7 + x8 + x9 + x10 + x11 + x12 
@@ -148,15 +148,6 @@ function fit_baseline_model(df_all::DataFrame, n_baseline_vars; verbose::Bool=fa
         linear_fm_2 = @formula(dim2 ~ 1 + t + measurement + 
             x1 + x2 + x3 + x4 + x5 + x6 + x7 + x8 + x9 + x10 + x11 + x12 
             + x13 + x14 + x15 + x16 + x17 + x18 + x19 + x20 + x21 + x22 + x23 + x24
-        )
-    elseif dataset == "risdiplam"
-        linear_fm_1 = @formula(dim1 ~ 1 + t + measurement + 
-            x1 + x2 + x3 + x4 + x5 + x6 + x7 + x8 + x9 + x10 + x11 + x12 
-            + x13 + x14 + x15 + x16 + x17 + x18 + x19 + x20 + x21 + x22 + x24 
-        )
-        linear_fm_2 = @formula(dim2 ~ 1 + t + measurement + 
-            x1 + x2 + x3 + x4 + x5 + x6 + x7 + x8 + x9 + x10 + x11 + x12 
-            + x13 + x14 + x15 + x16 + x17 + x18 + x19 + x20 + x21 + x22 +x24
         )
     elseif dataset == "zolgensma"
         linear_fm_1 = @formula(dim1 ~ 1 + t + measurement + 
@@ -168,6 +159,7 @@ function fit_baseline_model(df_all::DataFrame, n_baseline_vars; verbose::Bool=fa
         + x15 + x17 + x18 + x20 + x22 + x23 + x24 
     )
     end
+    # calculate squared errors
     lm_dim1 = fit(LinearModel, linear_fm_1, lm_df)
     lmfit_dim1 = predict(lm_dim1)
     lmprederr1 = sum((lmfit_dim1 .- lm_df[:,:dim1]).^2)
@@ -176,6 +168,7 @@ function fit_baseline_model(df_all::DataFrame, n_baseline_vars; verbose::Bool=fa
     lmfit_dim2 = predict(lm_dim2)
     lmprederr2 = sum((lmfit_dim2 .- lm_df[:,:dim2]).^2)
 
+    # gather predictions in dataframe
     prederrdf = DataFrame(
         Dimension = ["dim1", "dim2"],
         ODE = [round(ODEprederr_dim1, digits=3), round(ODEprederr_dim2, digits=3)],
@@ -184,177 +177,3 @@ function fit_baseline_model(df_all::DataFrame, n_baseline_vars; verbose::Bool=fa
 
     return prederrdf
 end
-#
-
-function get_reconstruction(m1::odevae, m2::odevae, X1, X2, Y, t1, t2, args::LossArgs; sample::Bool=false)
-
-    latentμ1, latentlogσ1 = m1.encodedμ(m1.encoder(X1)), m1.encodedlogσ(m1.encoder(X1))
-    #rulm latent variables
-    latentμ2, latentlogσ2 = m2.encodedμ(m2.encoder(X2)), m2.encodedlogσ(m2.encoder(X2))
-    #from baseline
-    params = vec(m1.ODEnet(Y))
-    ODEparams = m1.dynamics(params)
-
-    lt1, lt2 = Int32(length(t1)), Int32(length(t2))
-    tvals = unique([t1;t2])
-    lt = length(tvals)
-    # find indices of t1 and t2 in tvals
-    t1inds = findall(x -> x ∈ t1, tvals)
-    t2inds = findall(x -> x ∈ t2, tvals)
-
-    solarray = [generalsolution(solveatt - [t1;t2][startind], hcat(latentμ1, latentμ2)[:,startind], ODEparams...)[1] for startind in 1:lt1+lt2, solveatt in tvals]
-    smoothμarray = hcat([get_smoothμ(targetind, [t1; t2], solarray, args.weighting, args.skipt0) for targetind in 1:lt]...)
-
-    if lt1 > 0
-        if args.firstonly
-            smoothμ1 = hcat([generalsolution(tp, latentμ1[:,1], ODEparams...)[1] for tp in t1]...)
-        else
-            smoothμ1 = smoothμarray[:,t1inds]
-        end
-        reconstructed_X1 = get_reconstruction_from_latentμ(m1, smoothμ1, latentlogσ1; sample=sample)
-    else
-        reconstructed_X1 = Matrix{Float32}(undef, size(X1,1), 0)
-    end
-
-    if lt2 > 0
-        if args.firstonly
-            smoothμ2 = hcat([generalsolution(tp, latentμ2[:,1], ODEparams...)[1] for tp in t2]...)
-        else
-            smoothμ2 = smoothμarray[:,t2inds]
-        end
-        reconstructed_X2 = get_reconstruction_from_latentμ(m2, smoothμ2, latentlogσ2; sample=sample)
-    else
-        reconstructed_X2 = Matrix{Float32}(undef, size(X2,1), 0)
-    end
-
-    return reconstructed_X1, reconstructed_X2
-end
-#
-
-function get_reconstruction_from_latentμ(m::odevae, latentμ, latentlogσ; 
-    sample::Bool=false)
-
-    z = sample ? latentz.(latentμ, latentlogσ) : latentμ
-
-    decodedμ = m.decodedμ(m.decoder(z))
-
-    if sample 
-        decodedlogσ = m.decodedlogσ(m.decoder(z))
-        reconstructed_X = rand.(Normal.(decodedμ, sqrt.(exp.(decodedlogσ))))
-    else
-        reconstructed_X = decodedμ
-    end
-
-    return reconstructed_X
-
-end
-#
-
-function get_reconstruction_without_ODE(m1::odevae, m2::odevae, X1, X2; 
-    sample::Bool=false)
-
-    # hfmse latent variables
-    latentμ1, latentlogσ1 = m1.encodedμ(m1.encoder(X1)), m1.encodedlogσ(m1.encoder(X1))
-    #rulm latent variables
-    latentμ2, latentlogσ2 = m2.encodedμ(m2.encoder(X2)), m2.encodedlogσ(m2.encoder(X2))
-
-    reconstructed_X1, reconstructed_X2 = [], []
-
-    if size(X1,2) > 0
-        reconstructed_X1 = get_reconstruction_from_latentμ(m1, latentμ1, latentlogσ1; sample=sample)
-    end
-
-    if size(X2,2) > 0
-        reconstructed_X2 = get_reconstruction_from_latentμ(m2, latentμ2, latentlogσ2; sample=sample)
-    end
-
-    return reconstructed_X1, reconstructed_X2
-end
-#
-
-function fit_mixed_model_on_sumscores(mixeddata::SMAMixedTestData; verbose::Bool=false)
-    # fit mixed model for each patient 
-    # get sum scores of each instrument 
-    sumscores_origlevel_1 = collect(vec(Int.(sum(xs[1:end,:], dims=1))) for xs in mixeddata.xs1)
-    sumscores_origlevel_2 = collect(vec(Int.(sum(xs[1:end,:], dims=1))) for xs in mixeddata.xs2)
-
-    mm_df_all = DataFrame(t = [], score = [], measurement = [], patient_id = [])
-    rec_scores_1 = []
-    rec_scores_2 = []
-
-    for ind in 1:length(mixeddata.ids)
-
-        verbose && @info mixeddata.ids[ind]
-
-        cursumscore1 = sumscores_origlevel_1[ind]
-        cursumscore2 = sumscores_origlevel_2[ind]
-
-        t1, t2 = mixeddata.tvals1[ind], mixeddata.tvals2[ind]
-        lt1, lt2 = Int32(length(t1)), Int32(length(t2))
-
-        overall_mean = mean([cursumscore1; cursumscore2])
-        mean1, mean2 = mean(cursumscore1), mean(cursumscore2)
-
-        aligned_score_1 = cursumscore1 .- mean1 .+ overall_mean
-        aligned_score_2 = cursumscore2 .- mean2 .+ overall_mean
-
-        mm_df = DataFrame(
-            t = [t1;t2],
-            score = [aligned_score_1; aligned_score_2],
-            measurement = [fill("1", lt1); fill("2", lt2)], 
-            patient_id = fill(mixeddata.ids[ind], lt1+lt2)
-        )
-
-        append!(mm_df_all, mm_df)
-
-        if all(mm_df[:,:score] .== mm_df[1,:score])
-            verbose && @info "All scores are the same for patient $(mixeddata.ids[ind])"
-            mmfit = fill(mm_df[1,:score], lt1+lt2)
-        else
-            try
-                mm_sumscore = fit(MixedModel, @formula(score ~ 1 + t + (1|measurement)), mm_df) # ranef(mm) gives random effects
-                mmfit = predict(mm_sumscore)
-            catch
-                mmfit = fill(mm_df[1,:score], lt1+lt2)
-            end
-        end
-        rec_score_1 = mmfit[1:lt1] .- overall_mean .+ mean1
-        rec_score_2 = mmfit[lt1+1:end] .- overall_mean .+ mean2
-
-        push!(rec_scores_1, rec_score_1)
-        push!(rec_scores_2, rec_score_2)
-    end
-    return mm_df_all, rec_scores_1, rec_scores_2
-end
-# check sum scores: for RULM, they don't include "itema", so that should be left out for pure RULM
-#sumscores_origlevel_from_preprocessing = collect(Vector{Int64}(sumscores[i]) for i in 1:length(sumscores))
-#sumscores_origlevel = collect(vec(Int.(sum(xs[2:end,:], dims=1))) for xs in testdata.xs)
-#@assert sumscores_origlevel == sumscores_origlevel_from_preprocessing
-
-#=
-tvals = unique([t1;t2])
-lt = length(tvals)
-# find indices of t1 and t2 in tvals
-t1inds = findall(x -> x ∈ t1, tvals)
-t2inds = findall(x -> x ∈ t2, tvals)
-
-# align scores
-scoremat = fill(-999, (lt, 2))
-scoremat[t1inds, 1] = cursumscore1
-scoremat[t2inds, 2] = cursumscore2
-
-not1inds = setdiff(1:lt, t1inds)
-not2inds = setdiff(1:lt, t2inds)
-
-scoremat[not1inds,1] = scoremat[not1inds,2]
-scoremat[not2inds,2] = scoremat[not2inds,1]
-
-aligned_scores = fill(0.0, size(scoremat))
-means = mean(scoremat, dims=1)
-diffs_to_mean = fill(0.0, size(scoremat))
-diffs_to_mean[:,1] = scoremat[:,1] .+ means[:,2]
-diffs_to_mean[:,2] = scoremat[:,2] .- means[:,1]
-diffs_to_mean = mapslices(x -> x .- vec(mean(scoremat, dims=1)), scoremat, dims=2)
-aligned_scores[:,1] = scoremat[:,1] .- diffs_to_mean[:,2] # subtract diffs to mean of other instrument
-aligned_scores[:,2] = scoremat[:,2] .- diffs_to_mean[:,1] # add diffs to mean of other instrument
-=#
