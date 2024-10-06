126e76e7d83fde81470d1ff5d9ee73a36aa7c853
Maren Hackenberg <maren@imbi.uni-freiburg.de>
2024-10-06 10:27:57 +0200

    added docstrings, simplify lossargs struct, simpliy functions accordingly, remove unused code

--- src/model.jl
+++ src/model.jl
@@ -4,9 +4,29 @@
 
 
 #------------------------------
-# define and initialize model 
+# define and initialize model
 #------------------------------
 
+"""
+    odevae
+
+Struct for an ODE-VAE model, with the following fields:
+- `p`: number of VAE input dimensions, i.e., number of time-dependent variables
+- `q`: number of input dimensions for the baseline neural net, i.e., number of baseline variables
+- `zdim`: number of latent dimensions
+- `ODEnet`: neural net to map baseline variables to individual-specific ODE parameters 
+    (number of ODE parameters depends on the ODE system specified by the `dynamics` function)
+- `encoder`: neural net to map input data to latent space
+- `encodedμ`: neural net layer parameterizing the mean of the latent space
+- `encodedlogσ`: neural net layer parameterizing the log variance of the latent space
+- `decoder`: neural net to map latent variable to reconstructed input data
+- `decodedμ`: neural net layer parameterizing the mean of the reconstructed input data
+- `decodedlogσ`: neural net layer parameterizing the log variance of the reconstructed input data
+- `dynamics`: one of `params_fullinhomogeneous`, `params_offdiagonalinhomogeneous`, 
+    `params_diagonalinhomogeneous`, `params_driftonly`, `params_fullhomogeneous`, 
+    `params_offdiagonalhomogeneous`, `params_diagonalhomogeneous`: function to map a parameter vector
+    (=the output of the `ODEnet`) to the system matrix and constant vector of the ODE system
+"""
 mutable struct odevae
     p::Int
     q::Int
@@ -21,6 +41,26 @@ mutable struct odevae
     dynamics::Function # either ODEprob or params for analytical solution function 
 end
 
+"""
+    ModelArgs
+
+Struct to store model arguments, can be constructed with keyword arguments to set the following fields:
+- `p`: number of VAE input dimensions, i.e., number of time-dependent variables
+- `q`: number of input dimensions for the baseline neural net, i.e., number of baseline variables
+- `zdim`: number of latent dimensions
+- `dynamics`: one of `params_fullinhomogeneous`, `params_offdiagonalinhomogeneous`, 
+    `params_diagonalinhomogeneous`, `params_driftonly`, `params_fullhomogeneous`, 
+    `params_offdiagonalhomogeneous`, `params_diagonalhomogeneous`: function to map a parameter vector
+    (=the output of the `ODEnet`) to the system matrix and constant vector of the ODE system
+- `seed`: random seed for reproducibility
+- `bottleneck`: whether to use a bottleneck layer in the `ODEnet` 
+    to reduce the number of effective parameters for higher-dimensional systems
+- `init_scaled`: whether to initialize the `ODEnet` with scaled weights
+- `scale_sigmoid`: scaling factor for the sigmoid function used to shift the ODE parameters 
+    to a sensible range, acting as a prior
+- `add_diagonal`: whether to add a diagonal transformation to output of the `ODEnet` to add
+    flexibility after the sigmoid transformation
+"""
 @with_kw struct ModelArgs
     p::Int
     q::Int
@@ -33,20 +73,51 @@ end
     add_diagonal::Bool=true
 end
 
+"""
+    LossArgs
+
+Struct to store loss arguments, can be constructed with keyword arguments to set the following fields:
+- `λ_μpenalty`: weight for the penalty that encourages consistency of the mean before and after solving the ODEs
+- `λ_adversarialpenalty`: weight for the adversarial penalty that encourages alignment of the two tests
+- `λ_variancepenalty`: weight for the penalty on the variance of the ODE estimator
+- `variancepenaltytype`: one of `:ratio_sum`, `:sum_ratio`, `:log_diff`: 
+    type of penalty on the variance of the ODE estimator
+- `variancepenaltyoffset`: offset used in the penalty on the variance of the latent space
+"""
 @with_kw struct LossArgs
     λ_μpenalty::Float32 = 0.0f0
-    λ_ODEparamspenalty::Float32 = 0.0f0
     λ_adversarialpenalty::Float32 = 0.0f0
     λ_variancepenalty::Float32 = 0.0f0
     variancepenaltytype::Symbol = :ratio_sum # :sum_ratio, log_diff
     variancepenaltyoffset::Float32 = 1.0f0
-    firstonly::Bool=false
-    weighting::Bool=false
-    skipt0::Bool=false
 end
 
+"""
+    downscaled_glorot_uniform(dims...) -> Array{Float32}
+
+Generates an array of random values initialized using a downscaled version of the Glorot uniform initialization method.
+
+# Arguments
+- `dims...`: A variable number of integer arguments specifying the dimensions of the output array.
+
+# Returns
+- `Array{Float32}`: An array of the specified dimensions, filled with random values drawn from a uniform distribution scaled down to have a smaller variance. The values are centered around zero.
+"""
 downscaled_glorot_uniform(dims...) = (rand(Float32, dims...) .- 0.5f0) .* sqrt(0.01f0/sum(dims)) # smaller weights initialisation
 
+"""
+    get_nODEparams(dynamics::Function) -> Int
+
+Get the number of ODE parameters for a given ODE system specified by the function `dynamics`.
+
+# Arguments
+- `dynamics::Function`: Function specifying the ODE system, one of `params_fullinhomogeneous`, 
+    `params_offdiagonalinhomogeneous`, `params_diagonalinhomogeneous`, `params_driftonly`, 
+    `params_fullhomogeneous`, `params_offdiagonalhomogeneous`, `params_diagonalhomogeneous`.
+
+# Returns
+- `Int`: Number of ODE parameters for the specified ODE system.
+"""
 function get_nODEparams(dynamics::Function)
     if dynamics ∈ [params_driftonly, params_diagonalhomogeneous]
         nODEparams = 2
@@ -61,6 +132,13 @@ function get_nODEparams(dynamics::Function)
 end
 
 # initialise model
+"""
+    odevae(modelargs::ModelArgs)
+
+Function to initialize the ODE-VAE model according to the arguments passed in `modelargs`.
+
+Returns an `odevae` model.
+"""
 function odevae(modelargs::ModelArgs)
     nODEparams = get_nODEparams(modelargs.dynamics)
     myinit = modelargs.init_scaled ? downscaled_glorot_uniform : Flux.glorot_uniform 
@@ -97,14 +175,19 @@ end
 # define penalties
 #------------------------------
 
-function ODEparams_penalty(params)
-    if length(params) > 4
-        return sum(params[1:4].^2)
-    else
-        return sum(params.^2)
-    end
-end
+"""
+    μ_penalty(datatvals, latentμ, ODEparams) -> Float32
 
+Compute the penalty that encourages consistency of the mean before and after solving the ODEs.
+
+# Arguments
+- `datatvals`: Vector of time points at which the data is observed
+- `latentμ`: Matrix of latent variables, with each column corresponding to a time point in `datatvals`
+- `ODEparams`: Tuple of the system matrix and constant vector of the ODE system
+
+# Returns
+- `Float32`: Penalty value
+"""
 function μ_penalty(datatvals, latentμ, ODEparams)
     penalty = 0.0f0
     for i in 1:size(latentμ,2) 
@@ -122,12 +205,48 @@ end
 # define model functions 
 #------------------------------
 
+"""
+    latentz(μ, logσ) -> Array{Float32}
+
+Sample latent variable `z` from the mean `μ` and log variance `logσ` using the reparameterization trick from VAE training.
+
+# Arguments
+- `μ`: Mean of the latent variable
+- `logσ`: Log variance of the latent variable
+
+# Returns
+- `Array{Float32}`: Sampled latent variable `z`
+"""
 latentz(μ, logσ) = μ .+ sqrt.(exp.(logσ)) .* randn(Float32,size(μ)...) # sample latent z,
 
+"""
+    kl_q_p(μ, logσ) -> Array{Float32}
+
+Compute the KL divergence between the approximate posterior `q` and a standard Normal prior `p` in the latent space.
+
+# Arguments
+- `μ`: Mean of the latent variable
+- `logσ`: Log variance of the latent variable
+
+# Returns
+- `Array{Float32}`: KL divergence between `q` and `p`
+"""
 kl_q_p(μ, logσ) = 0.5f0 .* sum(exp.(logσ) + μ.^2 .- 1.0f0 .- (logσ),dims=1)
 
 #logp_x_z(m::odevae, x, z) = sum(logpdf.(Normal.(m.decodedμ(m.decoder(z)), sqrt.(exp.(m.decodedlogσ(m.decoder(z))))), x),dims=1) # get reconstruction error
+"""
+    logp_x_z(m::odevae, x, z) -> Array{Float32}
 
+Compute the log likelihood of the data `x` given the latent variable `z` and the ODE-VAE model `m`.
+
+# Arguments
+- `m::odevae`: ODE-VAE model
+- `x`: Data
+- `z`: Latent variable
+
+# Returns
+- `Array{Float32}`: Log likelihood of the data given the latent variable
+"""
 function logp_x_z(m::odevae, x::AbstractVecOrMat{S}, z::AbstractVecOrMat{S}) where S <: Real 
     μ = m.decodedμ(m.decoder(z))
     logσ = m.decodedlogσ(m.decoder(z))
@@ -135,11 +254,51 @@ function logp_x_z(m::odevae, x::AbstractVecOrMat{S}, z::AbstractVecOrMat{S}) whe
     return sum(res, dims=1)
 end
 
+"""
+    sqnorm(x) -> Float32
+
+Compute the squared norm of the input vector `x`.
+"""
 sqnorm(x) = sum(abs2, x)
+
+"""
+    reg(m::odevae) -> Float32
+
+Compute the squared norm of the decoder parameters as regularisation term in the loss function for the ODE-VAE model `m`.
+
+# Arguments
+- `m::odevae`: ODE-VAE model
+
+# Returns
+- `Float32`: Regularisation term in loss
+"""
 reg(m::odevae) = sum(sqnorm, Flux.params(m.decoder,m.decodedμ,m.decodedlogσ)) # regularisation term in loss
 
+"""
+    getparams(m::odevae) -> Flux.Params
+
+Collect the parameters of the ODE-VAE model `m` into a `Flux.Params` object.
+
+# Arguments
+- `m::odevae`: ODE-VAE model
+
+# Returns
+- `Flux.Params`: Parameters of the ODE-VAE model
+"""
 getparams(m::odevae) = Flux.params(m.encoder, m.encodedμ, m.encodedlogσ, m.decoder, m.decodedμ, m.decodedlogσ, m.ODEnet) # get parameters of VAE model
 
+"""
+    getparams(m1::odevae, m2::odevae) -> Flux.Params
+
+Collect the parameters of the ODE-VAE models `m1` and `m2` into a `Flux.Params` object.
+
+# Arguments
+- `m1::odevae`: first ODE-VAE model
+- `m2::odevae`: second ODE-VAE model
+
+# Returns
+- `Flux.Params`: Parameters of the ODE-VAE models
+"""
 function getparams(m1::odevae, m2::odevae) 
     Flux.params(m1.encoder, m1.encodedμ, m1.encodedlogσ, 
                 m1.decoder, m1.decodedμ, m1.decodedlogσ, 
@@ -149,7 +308,25 @@ function getparams(m1::odevae, m2::odevae)
     ) # get parameters of VAE model
 end
 
-function get_reconstruction(m::odevae, X, Y, t, args::LossArgs; sample::Bool=false)
+"""
+    get_reconstruction(m::odevae, X, Y, t, args::LossArgs; sample::Bool=false) -> Array{Float32}
+
+Compute the decoder reconstruction of the input data `X` and baseline variables `Y`, at observed time points `t`,
+using the ODE-VAE model `m`. 
+
+# Arguments
+- `m::odevae`: ODE-VAE model
+- `X`: time-dependent data
+- `Y`: Baseline variables
+- `t`: Time points
+
+# Keyword Arguments
+- `sample::Bool=false`: Whether to sample the latent variable or use the mean for decoding
+
+# Returns
+- `Array{Float32}`: Decoder reconstruction of the input data
+"""
+function get_reconstruction(m::odevae, X, Y, t; sample::Bool=false)
     latentμ, latentlogσ = m.encodedμ(m.encoder(X)), m.encodedlogσ(m.encoder(X))
     params = vec(m.ODEnet(Y))
     ODEparams = m.dynamics(params)
@@ -158,7 +335,7 @@ function get_reconstruction(m::odevae, X, Y, t, args::LossArgs; sample::Bool=fal
     else
         @assert length(t) == size(latentμ,2)
         solarray = [get_solution(startind, targetind, t, latentμ, ODEparams) for startind in 1:length(t), targetind in 1:length(t)]
-        smoothμ = hcat([get_smoothμ(targetind, t, solarray, args.weighting, args.skipt0) for targetind in 1:length(t)]...)
+        smoothμ = hcat([get_smoothμ(targetind, t, solarray, true, true) for targetind in 1:length(t)]...)
     end
     if sample
         z = latentz.(smoothμ, latentlogσ)
@@ -174,12 +351,45 @@ function get_reconstruction(m::odevae, X, Y, t, args::LossArgs; sample::Bool=fal
     return reconstructed_X
 end
 
+"""
+    get_solution(startind, targetind, tvals, latentμ, ODEparams) -> Array{Float32}
+
+Compute the solution of a linear ODE system for the latent variable `latentμ` at time points `tvals`,
+starting from `tvals[startind]` and ending at `tvals[targetind]` using the ODE parameters `ODEparams`.
+This function calls the explicit analytical solution of the ODE system.
+
+# Arguments
+- `startind`: Index of the starting time point
+- `targetind`: Index of the ending time point
+- `tvals`: Vector of time points
+- `latentμ`: Matrix of latent variables
+- `ODEparams`: Tuple of the system matrix and constant vector of the ODE system
+
+# Returns
+- `Array{Float32}`: Solution of the ODE system
+"""
 function get_solution(startind, targetind, tvals, latentμ, ODEparams)
     solveatt = tvals[targetind]
     tstart = tvals[startind]
     return generalsolution(solveatt - tstart, latentμ[:,startind], ODEparams...)[1]
 end
 
+
+"""
+    get_smoothμ(targetind, tvals, solarray, weighting::Bool, skipt0::Bool) -> Array{Float32}
+
+Compute the smoothed latent variable according to the ODE solution at time point `tvals[targetind]` using the solutions in `solarray`.
+
+# Arguments
+- `targetind`: Index of the target time point
+- `tvals`: Vector of observed time points
+- `solarray`: Array of solutions of the ODE system
+- `weighting::Bool`: Whether to weight the predictions using inverse variance weights
+- `skipt0::Bool`: Whether to skip the first time point 
+
+# Returns
+- `Array{Float32}`: Smoothed latent variable
+"""
 function get_smoothμ(targetind::Int, tvals::Vector{Float32}, solarray, weighting::Bool, skipt0::Bool)
     weightedpred = sum_weights = zero(solarray[1,1])
     for startind in 1:length(tvals)
@@ -207,16 +417,21 @@ end
 #------------------------------
 
 """
-    loss(X, Y, t, m::odevae; args::LossArgs)
+    loss(X1, X2, Y, t1, t2, m1::odevae, m2::odevae; args::LossArgs) -> Float32
 
-Compute the loss of the ODE-VAE model `m` on a batch of data, consisting of 
-    time-dependent variables `X`, baseline variables `Y` and time point `t`. 
+Compute the joint loss of the ODE-VAE models `m1` and `m2` on a batch of data, consisting of 
+    time-dependent variables of the first measurement instrument `X1` and of the second 
+    measurement instrument `X2`, together the the patient's baseline variables `Y` and 
+    observation time points `t1` of the first measurement instrument, and `t2` of the second 
+    measurement instrument.  
 
 Details of the loss function behaviour, including additional penalties, are controlled by the 
     keyword arguments `args` of type `LossArgs`, see `?LossArgs` for details.
 
-Returns the mean ELBO, where the ODE estimator of the underlying trajectory is used to decode the latent 
-    value at the time points `t` and obtain a reconstruction according to these smooth latent dynamics 
+Returns the sum of the mean ELBOs of `m1` and `m2`, where the ODE estimator of the underlying 
+    joint trajectory based on the latent representations of `X1` and `X2` is used as input for 
+    the decoder reconstructions of `m1` and `m2`, respectively, to obtain reconstructions of 
+    `X1` at time points `t1` and of `X2` at `t2`, according to these smooth latent dynamics 
     as specified by the ODE system.
 """
 function loss(X1, X2, Y, t1, t2, m1::odevae, m2::odevae; args::LossArgs)
@@ -237,40 +452,25 @@ function loss(X1, X2, Y, t1, t2, m1::odevae, m2::odevae; args::LossArgs)
     t2inds = findall(x -> x ∈ t2, tvals)
 
     solarray = [generalsolution(solveatt - [t1;t2][startind], hcat(latentμ1, latentμ2)[:,startind], ODEparams...)[1] for startind in 1:lt1+lt2, solveatt in tvals]
-    smoothμarray = hcat([get_smoothμ(targetind, [t1; t2], solarray, args.weighting, args.skipt0) for targetind in 1:lt]...)
+    smoothμarray = hcat([get_smoothμ(targetind, [t1; t2], solarray, true, true) for targetind in 1:lt]...)
     #solarray = [get_solution(startind, targetind, [t1;t2], hcat(latentμ1, latentμ2), ODEparams) for startind in 1:lt1+lt2, targetind in 1:lt1+lt2]
 
     if lt1 > 0
-        if args.firstonly
-            smoothμ1 = hcat([generalsolution(tp, latentμ1[:,1], ODEparams...)[1] for tp in t1]...)
-        else
-            #smoothμ1 = hcat([get_smoothμ(solveatt, [t1;t2], [latentμ1 latentμ2], [latentlogσ1 latentlogσ2], ODEparams, args.weighting, args.skipt0) for solveatt in t1]...)
-            smoothμ1 = smoothμarray[:,t1inds]
-        end
+        smoothμ1 = smoothμarray[:,t1inds]
         z1 = latentz.(smoothμ1, latentlogσ1)
         ELBO1 = 1.0f0 .* logp_x_z(m1, X1, z1) .- 0.5f0 .* kl_q_p(smoothμ1, latentlogσ1)
         lossval1 = mean(-ELBO1) + 0.01f0*reg(m1)
     end
     if lt2 > 0
-        if args.firstonly
-            smoothμ2 = hcat([generalsolution(tp, latentμ2[:,1], ODEparams...)[1] for tp in t2]...)
-        else
-            #smoothμ2 = hcat([get_smoothμ(solveatt, [t1;t2], [latentμ1 latentμ2], [latentlogσ1 latentlogσ2], ODEparams, args.weighting, args.skipt0) for solveatt in t2]...
-            smoothμ2 = smoothμarray[:,t2inds]
-        end
+        smoothμ2 = smoothμarray[:,t2inds]
         z2 = latentz.(smoothμ2, latentlogσ2)
         ELBO2 = 1.0f0 .* logp_x_z(m2, X2, z2) .- 0.5f0 .* kl_q_p(smoothμ2, latentlogσ2)
         lossval2 = mean(-ELBO2) + 0.01f0*reg(m2) # sum before 
     end
     penalties = 0.0f0
     if args.λ_μpenalty > 0.0f0
-        #penalties += !isempty(t1) && args.λ_μpenalty * μ_penalty(t1, latentμ1, ODEparams)
-        #penalties += !isempty(t2) && args.λ_μpenalty + μ_penalty(t2, latentμ2, ODEparams)
         penalties += !isempty(t1) && args.λ_μpenalty * sqrt.(sum(latentμ1 .- smoothμ1).^2)
         penalties += !isempty(t2) && args.λ_μpenalty * sqrt.(sum(latentμ2 .- smoothμ2).^2)
-    end
-    if args.λ_ODEparamspenalty > 0.0f0
-        penalties += args.λ_ODEparamspenalty * ODEparams_penalty(params)
     end
     if args.λ_adversarialpenalty > 0.0f0
         diff1 = isempty(t1) ? 0.0f0 : Float32(1.0f0/lt1).*sum(smoothμ1 .- latentμ1,dims=2)
