e2e363abfb44565b75a039cb869e6b646134e0ba
Maren Hackenberg <maren@imbi.uni-freiburg.de>
2024-10-06 10:28:19 +0200

    added docstrings, changed to new function names

--- src/modify_data.jl
+++ src/modify_data.jl
@@ -3,12 +3,31 @@
 #--------------------------------------------------------------------------------
 
 # 1) delete items at random time points 
+"""
+    delete_at_random(test, baseline_df, timedepend_df,
+        subscale2_names, p_dropout, pathname)
+        
+Delete items of test `test` in `timedepent_df` at random time points with probability `p_dropout`,
+using only the items in `subscale2_names`.
+
+# Arguments
+- `test::String`: test name
+- `baseline_df::DataFrame`: dataframe with baseline data
+- `timedepend_df::DataFrame`: dataframe with time-dependent data
+- `subscale2_names::Array{String}`: names of the items to be deleted
+- `p_dropout::Float64`: probability of dropout
+- `pathname::String`: path to save the modified data
+
+# Returns
+- `recoded_testdata2::SMATestData`: object containing the modified data in the form of a `SMATestData` object
+- `pathname::String`: path to save the modified data
+"""
 function delete_at_random(test, baseline_df, timedepend_df, 
     subscale2_names, p_dropout, pathname)
 
     # delete items at random time points 
 
-    testdata2 = get_SMArtCARE_data(test, baseline_df, timedepend_df, var_names=subscale2_names);
+    testdata2 = get_SMArtCARE_data_one_test(test, baseline_df, timedepend_df, var_names=subscale2_names);
     xs2, xs_baseline2, tvals2, ids2 = testdata2.xs, testdata2.xs_baseline, testdata2.tvals, testdata2.ids;
     recoded_testdata2 = recode_SMArtCARE_data(testdata2);
     
@@ -32,12 +51,31 @@ function delete_at_random(test, baseline_df, timedepend_df,
 end
 
 # 2) delete items at later time points preferentially 
+"""
+    delete_at_later_tps(test, baseline_df, timedepend_df, 
+        subscale2_names, shrink_prob_offset, pathname)
+
+Delete items of test `test` in `timedepent_df` at later time points preferentially, using only the items in `subscale2_names`.
+The probability of deleting an item at time point number `t` is `t/(length(curtvals) + shrink_prob_offset)`.
+
+# Arguments
+- `test::String`: test name
+- `baseline_df::DataFrame`: dataframe with baseline data
+- `timedepend_df::DataFrame`: dataframe with time-dependent data
+- `subscale2_names::Array{String}`: names of the items to be deleted
+- `shrink_prob_offset::Int`: offset for the probability of deletion
+- `pathname::String`: path to save the modified data
+
+# Returns
+- `recoded_testdata2::SMATestData`: object containing the modified data in the form of a `SMATestData` object
+- `pathname::String`: path to save the modified data
+"""
 function delete_at_later_tps(test, baseline_df, timedepend_df, 
     subscale2_names, shrink_prob_offset, pathname)
     
     # delete items at later time points preferentially 
 
-    testdata2 = get_SMArtCARE_data(test, baseline_df, timedepend_df, var_names=subscale2_names);
+    testdata2 = get_SMArtCARE_data_one_test(test, baseline_df, timedepend_df, var_names=subscale2_names);
     xs2, xs_baseline2, tvals2, ids2 = testdata2.xs, testdata2.xs_baseline, testdata2.tvals, testdata2.ids;
     recoded_testdata2 = recode_SMArtCARE_data(testdata2);
 
@@ -63,12 +101,30 @@ function delete_at_later_tps(test, baseline_df, timedepend_df,
 end
 
 # 3) apply a shift for all patients and items
+"""
+    shift_all_patients_items(test, baseline_df, timedepend_df, 
+        subscale2_names, shift, pathname)
+
+Apply a shift for all patients and items in test `test` in `timedepent_df`, using only the items in `subscale2_names`.
+
+# Arguments
+- `test::String`: test name
+- `baseline_df::DataFrame`: dataframe with baseline data
+- `timedepend_df::DataFrame`: dataframe with time-dependent data
+- `subscale2_names::Array{String}`: names of the items to be shifted
+- `shift::Float64`: shift value
+- `pathname::String`: path to save the modified data
+
+# Returns
+- `recoded_testdata2::SMATestData`: object containing the modified data in the form of a `SMATestData` object
+- `pathname::String`: path to save the modified data
+"""
 function shift_all_patients_items(test, baseline_df, timedepend_df, 
     subscale2_names, shift, pathname)
         
     # apply a shift for all patients and items
 
-    testdata2 = get_SMArtCARE_data(test, baseline_df, timedepend_df, var_names=subscale2_names);
+    testdata2 = get_SMArtCARE_data_one_test(test, baseline_df, timedepend_df, var_names=subscale2_names);
     xs2, xs_baseline2, tvals2, ids2 = testdata2.xs, testdata2.xs_baseline, testdata2.tvals, testdata2.ids;
     recoded_testdata2 = recode_SMArtCARE_data(testdata2);
 
@@ -82,12 +138,31 @@ function shift_all_patients_items(test, baseline_df, timedepend_df,
 end
 
 # 4) apply a shift for a random subgroup of patients 
+"""
+    shift_random_subgrop(test, baseline_df, timedepend_df, 
+        subscale2_names, shift, p_subgroup, pathname)
+
+Apply a shift for a random subgroup of patients in test `test` in `timedepend_df`, using only the items in `subscale2_names`.
+
+# Arguments
+- `test::String`: test name
+- `baseline_df::DataFrame`: dataframe with baseline data
+- `timedepend_df::DataFrame`: dataframe with time-dependent data
+- `subscale2_names::Array{String}`: names of the items to be shifted
+- `shift::Float64`: shift value
+- `p_subgroup::Float64`: probability of a patient to be part of the shifted subgroup
+- `pathname::String`: path to save the modified data
+
+# Returns
+- `recoded_testdata2::SMATestData`: object containing the modified data in the form of a `SMATestData` object
+- `pathname::String`: path to save the modified data
+"""
 function shift_random_subgrop(test, baseline_df, timedepend_df, 
     subscale2_names, shift, p_subgroup, pathname)
 
     # apply a shift for a random subgroup of patients 
 
-    testdata2 = get_SMArtCARE_data(test, baseline_df, timedepend_df, var_names=subscale2_names);
+    testdata2 = get_SMArtCARE_data_one_test(test, baseline_df, timedepend_df, var_names=subscale2_names);
     xs2, xs_baseline2, tvals2, ids2 = testdata2.xs, testdata2.xs_baseline, testdata2.tvals, testdata2.ids;
     recoded_testdata2 = recode_SMArtCARE_data(testdata2);
 
@@ -110,12 +185,33 @@ function shift_random_subgrop(test, baseline_df, timedepend_df,
 end
 
 # 5) delete items at later time points if the sum score is above a certain threshold 
+"""
+    delete_later_above_threshold(test, baseline_df, timedepend_df, 
+        subscale2_names, sumscores, sumscore_cutoff, pathname)
+
+Delete items of test `test` in `timedepent_df` at later time points if the sum score 
+of the other test is above a certain threshold, defined by `sumscore_cutoff`, 
+using only the items in `subscale2_names`.
+
+# Arguments
+- `test::String`: test name
+- `baseline_df::DataFrame`: dataframe with baseline data
+- `timedepend_df::DataFrame`: dataframe with time-dependent data
+- `subscale2_names::Array{String}`: names of the items to be deleted
+- `sumscores::Array{Array{Float64}}`: sum scores for each patient
+- `sumscore_cutoff::Float64`: sum score threshold
+- `pathname::String`: path to save the modified data
+
+# Returns
+- `recoded_testdata2::SMATestData`: object containing the modified data in the form of a `SMATestData` object
+- `pathname::String`: path to save the modified data
+"""
 function delete_later_above_threshold(test, baseline_df, timedepend_df, 
     subscale2_names, sumscores, sumscore_cutoff, pathname)
 
     # delete items at later time points if the sum score is above a certain threshold 
 
-    testdata2 = get_SMArtCARE_data(test, baseline_df, timedepend_df, var_names=subscale2_names);
+    testdata2 = get_SMArtCARE_data_one_test(test, baseline_df, timedepend_df, var_names=subscale2_names);
     xs2, xs_baseline2, tvals2, ids2 = testdata2.xs, testdata2.xs_baseline, testdata2.tvals, testdata2.ids;
     recoded_testdata2 = recode_SMArtCARE_data(testdata2);
 
@@ -147,12 +243,33 @@ function delete_later_above_threshold(test, baseline_df, timedepend_df,
 end
 
 # 6) delete items at earlier time points if the sum score is above a certain threshold 
+"""
+    delete_earlier_above_threshold(test, baseline_df, timedepend_df, 
+        subscale2_names, sumscores, sumscore_cutoff, pathname)
+
+Delete items of test `test` in `timedepent_df` at earlier time points if the sum score of 
+the other test is above a certain threshold, defined by `sumscore_cutoff`, 
+using only the items in `subscale2_names`.
+
+# Arguments
+- `test::String`: test name
+- `baseline_df::DataFrame`: dataframe with baseline data
+- `timedepend_df::DataFrame`: dataframe with time-dependent data
+- `subscale2_names::Array{String}`: names of the items to be deleted
+- `sumscores::Array{Array{Float64}}`: sum scores for each patient
+- `sumscore_cutoff::Float64`: sum score threshold
+- `pathname::String`: path to save the modified data
+
+# Returns
+- `recoded_testdata2::SMATestData`: object containing the modified data in the form of a `SMATestData` object
+- `pathname::String`: path to save the modified data
+"""
 function delete_earlier_above_threshold(test, baseline_df, timedepend_df, 
     subscale2_names, sumscores, sumscore_cutoff, pathname)
         
     # delete items at earlier time points if the sum score is above a certain threshold 
 
-    testdata2 = get_SMArtCARE_data(test, baseline_df, timedepend_df, var_names=subscale2_names);
+    testdata2 = get_SMArtCARE_data_one_test(test, baseline_df, timedepend_df, var_names=subscale2_names);
     xs2, xs_baseline2, tvals2, ids2 = testdata2.xs, testdata2.xs_baseline, testdata2.tvals, testdata2.ids;
     recoded_testdata2 = recode_SMArtCARE_data(testdata2);
 
@@ -187,6 +304,40 @@ function delete_earlier_above_threshold(test, baseline_df, timedepend_df,
     return recoded_testdata2, pathname
 end
 
+"""
+    modify_data(test, baseline_df, timedepend_df, subscale2_names, sumscores, mod_no, 
+        p_dropout, shrink_prob_offset, shift, p_subgroup, sumscore_cutoff, pathname)
+
+Modify the data according to the specified modification number `mod_no`. 
+This function calls the corresponding function to apply the modification: 
+
+- `mod_no = 1`: delete items at random time points (see `delete_at_random`)
+- `mod_no = 2`: delete items at later time points preferentially (see `delete_at_later_tps`)
+- `mod_no = 3`: apply a shift for all patients and items (see `shift_all_patients_items`)
+- `mod_no = 4`: apply a shift for a random subgroup of patients (see `shift_random_subgrop`)
+- `mod_no = 5`: delete items at later time points if the sum score of the other test 
+            is above a certain threshold (see `delete_later_above_threshold`)
+- `mod_no = 6`: delete items at earlier time points if the sum score of the other test 
+            is above a certain threshold (see `delete_earlier_above_threshold`)
+
+# Arguments
+- `test::String`: test name
+- `baseline_df::DataFrame`: dataframe with baseline data
+- `timedepend_df::DataFrame`: dataframe with time-dependent data
+- `subscale2_names::Array{String}`: names of the items to be modified
+- `sumscores::Array{Array{Float64}}`: sum scores for each patient
+- `mod_no::Int`: modification number
+- `p_dropout::Float64`: probability of dropout
+- `shrink_prob_offset::Int`: offset for the probability of deletion
+- `shift::Float64`: shift value
+- `p_subgroup::Float64`: probability of a patient to be part of the shifted subgroup
+- `sumscore_cutoff::Float64`: sum score threshold
+- `pathname::String`: path to save the modified data
+
+# Returns
+- `recoded_testdata2::SMATestData`: object containing the modified data in the form of a `SMATestData` object
+- `pathname::String`: path to save the modified data
+"""
 function modify_data(test, 
     baseline_df, timedepend_df, subscale2_names, sumscores, mod_no, 
     p_dropout, shrink_prob_offset, shift, p_subgroup, sumscore_cutoff, 
